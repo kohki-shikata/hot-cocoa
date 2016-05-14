1d0d221b95b8221c54e192a459d0abf4e951424b
kohki-shikata <indigoworks@gmail.com>
2016-05-14 15:02:38 +0900

    Initial commit

---
+++ dev/gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+// var ejs = require('gulp-ejs');
+var pug = require('gulp-pug');
+var browserSync = require('browser-sync').create();
+
+var sassPaths = [
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
+];
+
+var dir = "../dist/"
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: dir
+        }
+    });
+});
+
+// gulp.task("ejs", function() {
+//     gulp.src(
+//         ["ejs/**/*.html",'!' + "ejs/**/_*.html"]
+//     )
+//         .pipe(ejs())
+//         .pipe(gulp.dest(dir))
+//     .pipe(browserSync.reload({stream:true}));
+// });
+
+gulp.task("pug", function() {
+    gulp.src(
+        ["pug/**/*.pug",'!' + "pug/**/_*.pug"]
+    )
+        .pipe(pug({pretty: true}))
+        .pipe(gulp.dest(dir))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+var copy_js = [
+  'bower_components/foundation-sites/dist/foundation.min.js',
+  'bower_components/jquery/dist/jquery.min.js',
+  'bower_components/motion-ui/dist/motion-ui.min.js',
+  'bower_components/what-input/what-input.min.js',
+  'bower_components/Slidebars/dist/slidebars.min.js',
+  'js/app.js',
+];
+
+var copy_css = [
+'bower_components/motion-ui/dist/motion-ui.min.css',
+];
+
+gulp.task('copy', function () {
+  gulp.src(copy_js)
+   .pipe(gulp.dest(dir + 'common/js'));
+  gulp.src(copy_css)
+   .pipe(gulp.dest(dir + 'common/css'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest(dir + 'common/css'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('default', ['browser-sync'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['ejs/**/*.html'], ['ejs']);
+  gulp.watch(['pug/**/*.pug'], ['pug']);
+});
